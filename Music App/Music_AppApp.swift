fb5d53ca268c8990ad9e6fcb99007ca992a1c179
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-11-28 09:33:51 -0600

    Initial Commit

---
+++ Music App/Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppApp.swift
+//  Music App
+//
+//  Created by Jonathan Varghese on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
